5abb44cd045dd5e7245209de22f1ee2943f77a3c
Анастасия Юрашева <anastasiauraseva@MacBook-Air-Anastasia.local>
2022-10-17 16:06:55 +0300

    fix: algorithmic tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -10,23 +10,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * const reverseMachine = new VigenereCipheringMachine(false);
  * 
- * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
+ * directMachine.encrypt('attack at dawn!', 'alphabethonse') => 'AEIHQX SX DLLU!'
  * 
- * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
+ * directMachine.decrypt('AEIHQX SX DLLU!', 'alphabethonse') => 'ATTACK AT DAWN!'
  * 
- * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
+ * reverseMachine.encrypt('attack at dawn!', 'alphabethonse') => '!ULLD XS XQHIEA'
  * 
- * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
+ * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphabethonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(direct = true) {
+    this.alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+		key = key.toUpperCase();
+    let index = 0;
+    let res = '';
+
+    for (let i = 0; i < message.length; i++) {
+      if (!this.alphabet.includes(message[i])) {
+        res += message[i];
+        continue;
+      }
+      res += this.alphabet[(this.alphabet.indexOf(message[i]) + this.alphabet.indexOf(key[index++ % key.length])) % this.alphabet.length];
+    }
+    return this.direct ? res : res.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    
+    message = message.toUpperCase();
+		key = key.toUpperCase();
+    let index = 0;
+    let res = '';
+
+    for (let i = 0; i < message.length; i++) {
+      if (!this.alphabet.includes(message[i])) {
+        res += message[i];
+        continue;
+      }
+
+      let dec = this.alphabet.indexOf(message[i]) - this.alphabet.indexOf(key[index++ % key.length]) % this.alphabet.length;
+
+      if (dec >= 0)
+        res += this.alphabet[dec];  
+      else
+        res += this.alphabet[this.alphabet.length + dec];
+    }
+    return this.direct ? res : res.split('').reverse().join('');
   }
 }
 
